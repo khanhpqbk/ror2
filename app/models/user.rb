362157b4ac30b8581cbd3aa1d9841d12a4800848
khanhpqbk <khanhpq.bk@gmail.com>
2015-03-24 16:11:04 +0700

    add password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :micropost
+	validates :name, presence: true
+	validates :email, presence: true
+
+	has_secure_password
+	validates :password, length: {minimum: 6}
 end
